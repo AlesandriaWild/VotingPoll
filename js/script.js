80928376cb16e3eba443949172255b25d3cf3af9
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-10 16:38:44 -0700

    add score lets, starting parcen
    Co-authored-by: Noah Cowan <nsncowan@gmail.com>

--- js/script.js
+++ js/script.js
@@ -5,22 +5,30 @@ window.onload = function() {
   form.onsubmit = function(e) {
     e.preventDefault();
     hideResults();
+    
+    let healthcare = document.getElementById("healthcare");
+    let money = document.getElementById("money");
+    let medicare  = document.getElementById("medicare");
+    let trump = document.getElementById("trump");
+    let abort = document.getElementById("abort");
+    let loans = document.getElementById("loans");
   
     let over50 = document.getElementById("over_50");
     over50.setAttribute("class", "hidden");
     let under50 = document.getElementById("under_50");
     under50.setAttribute("class", "hidden");
-  
+    let independent = document.getElementById("thirty");
+    thirty.setAttribute("class,hidden");
+
     const age = parseInt(document.querySelector("input#age").value);
-    let 
+    const score = add("healthcare" + "money" + "medicare" + "trump" + "abort" + "loans");
   
-    if (age >= 50) {
+    if (age >= 50 && score <= 30) {
       over50.removeAttribute("class");
-    } 
-      if (age <=50 &&  )
+    }
       else {
   
-      under18.removeAttribute("class");
+      under50.removeAttribute("class");
   
       }
     };
