b95f3deab31b022c3e150cf5b7329f9b60b34cd2
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-10 17:17:00 -0700

    WIP showing other poltical parties than liberal
    Co-authored-by: Noah Cowan <nsncowan@gmail.com>

--- js/script.js
+++ js/script.js
@@ -4,32 +4,36 @@ window.onload = function() {
   
   form.onsubmit = function(e) {
     e.preventDefault();
-    hideResults();
-    
-    let healthcare = document.getElementById("healthcare");
-    let money = document.getElementById("money");
-    let medicare  = document.getElementById("medicare");
-    let trump = document.getElementById("trump");
-    let abort = document.getElementById("abort");
-    let loans = document.getElementById("loans");
+
+    let healthcare = parseInt(document.getElementById("healthcare"));
+    let money = parseInt(document.getElementById("money"));
+    let medicare  = parseInt(document.getElementById("medicare"));
+    let trump = parseInt(document.getElementById("trump"));
+    let abort = parseInt(document.getElementById("abort"));
+    let loans = parseInt(document.getElementById("loans"));
   
     let over50 = document.getElementById("over_50");
     over50.setAttribute("class", "hidden");
     let under50 = document.getElementById("under_50");
     under50.setAttribute("class", "hidden");
     let independent = document.getElementById("thirty");
-    thirty.setAttribute("class,hidden");
+    thirty.setAttribute("class", "hidden");
 
     const age = parseInt(document.querySelector("input#age").value);
-    const score = add("healthcare" + "money" + "medicare" + "trump" + "abort" + "loans");
-  
+    const score = healthcare + money + medicare + trump + abort + loans;
+    console.log (score)
+
+
     if (age >= 50 && score <= 30) {
       over50.removeAttribute("class");
     }
-      else {
-  
-      under50.removeAttribute("class");
-  
+      else if (score === 30) { 
+        thirty.removeAttribute("class");
       }
+    
+      else {
+      under50.removeAttribute("class");
+      }
+
     };
   };
